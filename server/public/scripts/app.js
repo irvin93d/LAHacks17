353965606d63993575b9cb64c5593d0618590190
Jonathan Ohlsson <joohls@kth.se>
2017-04-01 15:03:48 -0700

    fiering setup

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,6 +1,7 @@
 let serverAddress = 'http://localhost:3000';
 
 var socket = io.connect(serverAddress);
+socket.emit('setup', {user: "Jonathan", country: "Sweden", profile: "www.facebook.com"});
 
 window.onload = () => {
     var input = new Vue({
