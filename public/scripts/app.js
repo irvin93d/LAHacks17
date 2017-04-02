fde288a8c4149bd58a2049cb8c53aabf8fcd1d14
Jonathan Ohlsson <joohls@kth.se>
2017-04-02 06:04:27 -0700

    hack

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -70,7 +70,7 @@ window.onload = () => {
         request: function() {
             console.log("requesting " + this.checkedNames);
             if(this.checkedNames.length) {
-                socket.emit('request contact', {roomID: this.roomID, names: this.checkedNames, me: user.nick});
+                socket.emit('request contact', {roomID: this.roomID, names: this.checkedNames, me: user.prevNick});
             }
             this.roomID = 0;
             this.requestContact = false;
@@ -146,6 +146,7 @@ window.onload = () => {
     });
     
     socket.on('nickname', function(nick) {
+        user.prevNick = user.nick;        
         chat.nick = nick;
         user.nick = nick;
     });
