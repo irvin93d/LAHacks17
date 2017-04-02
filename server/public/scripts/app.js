c762511a3ff2b630f628677bd1a02567740f1e12
Jonathan Ohlsson <joohls@kth.se>
2017-04-01 17:10:13 -0700

    soz

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,20 @@
 let serverAddress = 'http://localhost:3000';
-
 var socket = io.connect(serverAddress);
+let state = {
+    messages: [
+            { text: 'Vegetables' },
+            { text: 'Cheese' },
+            { text: 'Whatever else humans are supposed to eat' }
+           ],
+    addMessage: function(m) {
+        this.messages.push(m);
+    }
+    };
 socket.emit('setup', {user: "Jonathan", country: "Sweden", profile: "www.facebook.com"});
 
 window.onload = () => {
+
+    // Chat input
     var input = new Vue({
     el: '#message-input',
     data: {
@@ -12,9 +23,24 @@ window.onload = () => {
     methods: {
         send: function() {
             console.log(this.message);
-            this.message = "";
+            state.addMessage({text: this.message});
             socket.emit('message', this.message);
+            this.message = "";
         }
     }
-    })
+    });
+
+    // Message list 
+    Vue.component('message-item', {
+        props: ['message'],
+        template: '<div>{{message.text}}</div>'
+    });
+
+    var messageList = new Vue({
+        el: '#message-list',
+        data: {
+            messages: state.messages
+        }
+    });
+
 }
